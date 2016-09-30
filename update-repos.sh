0a9f426cc76cca9aeb046a44f62359b592f000b0
Cory Collier <cory.collier@ahss.org>
2016-09-30 10:53:46 -0400

    updating the script to update all repos

--- update-repos.sh
+++ update-repos.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-REPOSITORY_DIR="${REPOSITORY_DIR:-$HOME/Repositories/}"
+# Iterates through given folders
+repo-cleaner-iterate() {
+    RESET="\033[0m"
+    for DIR in "$@";
+    do
+        cd "$DIR/../"
+        BRANCH="$(git name-rev --name-only HEAD)"
+        echo -e "${RESET}====================================================================="
+        echo "$DIR [$BRANCH]"
+        repo-cleaner-update
+    done
+}
 
-for DIR in $(find $REPOSITORY_DIR* -maxdepth 0 -type d );
-do
-    cd $DIR
-    BRANCH="$(git name-rev --name-only HEAD)"
-    echo "====================================================================="
-    echo "$DIR [$BRANCH]"
+# Updates all of the git stuff in a folder
+repo-cleaner-update() {
+    RED="\033[01;31m"
 
     git fetch origin
     git pull origin $BRANCH
-    git remote prune origin
+    echo -e "\n${RED}git remote prune origin"
     git prune
     git gc --aggressive
     git fsck --full
-done
+}
+
+repo-cleaner-iterate $(find $HOME -name .git -type d)
